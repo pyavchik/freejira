9d304d5715b85d6ccdaf4712fb3b3760ce0a271d
Oleksander Pyavchik <pyavchik@gmail.com>
2025-12-26 23:49:04 +0200

    Add TypeScript type definitions for Google OAuth API

---
+++ frontend/types/google.d.ts
@@ -0,0 +1,21 @@
+interface Window {
+  google?: {
+    accounts: {
+      id: {
+        initialize: (config: {
+          client_id: string | undefined
+          callback: (response: { credential: string }) => void
+        }) => void
+        renderButton: (
+          element: HTMLElement | null,
+          options: {
+            theme: string
+            size: string
+            width?: string
+            locale?: string
+          }
+        ) => void
+      }
+    }
+  }
+}
